198b053246bcf78370872afda0046fc3fa7bf6ef
smaii <smaiifakher1994@gmail.com>
2022-04-19 13:15:08 +0100

    Initial commit

---
+++ src/main/java/com/onpc/amsdata/AmsDataApplication.java
@@ -0,0 +1,13 @@
+package com.onpc.amsdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmsDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onpc/amsdata/AmsDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onpc.amsdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmsDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
